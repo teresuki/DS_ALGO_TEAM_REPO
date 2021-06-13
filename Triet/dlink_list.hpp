28da0cac75e767100ffff2a80776df782df0580e
Triet <triet403@gmail.com>
2021-06-13 17:31:42 +0700

    working linked list and WIP hash table

---
+++ Triet/dlink_list.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+using namespace std;
+
+struct Node
+{
+	float value;
+	Node *next ;
+	Node *previous ;
+	Node(int value): value(value) {}
+};
+
+// Node **head needs to be a double pointer because it is pointing to Node *head on stack which is already a pointer
+void insert_after(Node **head, float new_value)
+{
+	//allcating a memory of a new node on heap
+	Node *new_node = new Node(new_value);
+
+	//if there isn't any node added to head yet
+	if( *head == nullptr)
+	{
+		(*head) = new_node;	
+		new_node->previous = nullptr;
+		new_node->next = nullptr;
+		return;
+	}
+
+	//if there is already a node added to the chain
+	if( *head != nullptr)
+	{
+		// we need to find the node that is the last node in the chain 
+		Node *last = *head;
+		while ( last->next != nullptr) // if the next node of the chain exist move to it
+		{
+			last = last->next;
+		}
+
+		last->next = new_node; // link the new node to the last node in the chain
+		new_node->previous = last; // connect previous to the last node
+		new_node->next = nullptr;
+		return;
+	}
+
+}
+
+// search for a node and return the address of the wanted node
+Node *search_node(Node **head, float wanted_value)
+{
+	Node *current = *head;
+	while( current->value != wanted_value && current->next != nullptr)
+	{
+		// cout <<"go in" <<'\n';
+		current = current->next;
+	}
+	if (current->value == wanted_value) return current;
+	else{
+		cout <<"Node with that value doesn't exist" <<'\n';
+		return nullptr;
+	} 
+}
+
+void delete_node(Node **head, float wanted_value)
+{
+	Node *current = *head;
+	Node *previous_node = nullptr;
+	Node *next_node = nullptr;
+	while( current->value != wanted_value && current->next != nullptr)
+	{
+		// cout <<"go in" <<'\n';
+		current = current->next;
+	}
+	if (current->value != wanted_value)
+	{
+		cout <<"Node with that value doesn't exist" <<'\n';
+		return;
+	} 
+
+	if( current->value == wanted_value)
+	{
+		//if the deleted node is in middle of the chain
+		if(current->previous != nullptr && current->next != nullptr)
+		{
+			previous_node = current->previous;
+			next_node = current->next;
+
+			previous_node->next = next_node;
+			next_node->previous = previous_node;
+
+			delete current;
+		}
+
+		//if deleted node is at the start of the chain
+		if(current->previous == nullptr && current->next != nullptr)
+		{
+			next_node = current->next;
+
+			*head = next_node;
+			next_node->previous = nullptr;
+
+			delete current;
+		}
+
+		//if deleted node is at the end of the chain
+		if(current->previous != nullptr && current->next == nullptr)
+		{
+			previous_node = current->previous;
+
+			previous_node->next = nullptr;
+
+			delete current;
+		}
+
+		//if deleted node is the only node in the chain
+		if( current->previous == nullptr && current->next == nullptr)
+		{
+			*head = nullptr;
+			delete current;
+		}
+	}
+}
+
+// display the linked list begin to end
+void display_dlink(Node *current)
+{
+	while(current != nullptr)
+	{
+		cout << current->value <<'\t';
+		current = current->next; // here we are changing the address of current's memory to the address value stored in (*current).next 
+	}
+	if( current == nullptr) cout <<"null";
+	putchar('\n');
+}
---
+++ Triet/hash_table.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <array>
+#include "dlink_list.hpp"
+#include <string.h>
+
+#define SIZE 3
+
+
+using namespace std;
+
+// A unit is each element that we need to put into the hash table
+struct Unit
+{
+	string key;
+	float value;
+	Unit(string key,float value): key(key), value(value) {}
+};	
+
+// return us the index of the hash table
+int hash_function(string key)
+{
+	//first we need to turn the string into ASCII value
+	int key_ASCII = 0;
+
+	for(int i =0; i < key.size() ; i++)
+	{
+		key_ASCII += static_cast<int>(key[i]);
+	}
+	key_ASCII = key_ASCII % SIZE;
+
+	return key_ASCII;
+}
+
+// add hash using linked list to avoid collision
+void add_hash_table(Unit input, array<Node *,SIZE> hash_table)
+{
+	// get the index
+	int index = hash_function(input.key);
+
+	
+	insert_after(&hash_table[index], input.value);
+}
+
+void print_hash_table(array<Node *,SIZE> hash_table)
+{
+	for(int i=0; i < hash_table.size(); i ++)
+	{
+		display_dlink(hash_table[i]);
+	}
+}
+
+int main()
+{
+	// create an array full of pointers to Node which will be out hash table
+	array<Node *,SIZE> hash_table;
+
+	// fill each elements in our hash table with nullptr
+	for(int i =0; i < hash_table.size(); i++)
+	{
+		Node *head = nullptr;
+		hash_table[i] = head;
+	}
+
+	Unit U1("abcd",1);
+	Unit U2("bbcd",2);
+	Unit U3("cbcd",3);
+	Unit U4("dbcd",4);
+
+	add_hash_table(U1,hash_table);
+	add_hash_table(U2,hash_table);
+	add_hash_table(U3,hash_table);
+	add_hash_table(U4,hash_table);
+
+	print_hash_table(hash_table);
+	return 0;
+}
