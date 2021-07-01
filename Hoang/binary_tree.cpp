a62203568e6646de401a6bace34a253ab0c35b6b
Minh Hoang <hoangdsieud@gmail.com>
2021-07-01 10:46:19 +0700

    Binary Tree updated
    added delete function into Binary Tree

--- Hoang/binary_tree.cpp
+++ Hoang/binary_tree.cpp
@@ -1,3 +1,10 @@
+/*
+ * Binary Search Tree
+ * 
+ * 1st July
+ * Teresuki
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -43,16 +50,16 @@ struct BinaryTree
         root = nullptr;
     }
 
-    Node* minNode()
+    Node* minNode(Node* node)
     {
-        Node* temp = root;
+        Node* temp = node;
         while(temp->leftChild())
          temp = temp->left;
         return temp;
     }
-    Node* maxNode()
+    Node* maxNode(Node* node)
     {
-        Node* temp = root;
+        Node* temp = node;
         while(temp->rightChild())
          temp = temp->right;
         return temp;
@@ -82,6 +89,98 @@ struct BinaryTree
             }
         }
      
+    }
+
+    //node should be root as the first parameter
+    //Encounter is used if you want to delete a node with duplicated key in the tree
+    void deleteNode(Node* node, int key, Node* parent = nullptr, int encounter = 1)
+    {
+        static int enc = 0;
+
+        if(key == node->key)
+        {
+            enc++;
+            if(enc < encounter) goto here;
+            //From Left, From Right
+            bool fLeft = false, fRight = false;
+            if(node == parent->left) fLeft = true;
+            if(node == parent->right) fRight = true;
+
+            if(fLeft)
+            {
+                //No children
+                if(node->leftChild() == 0 && node->rightChild() == 0)
+                {
+                    parent->left = nullptr;
+                }
+                //Left Child
+                else if(node->leftChild() == 1 && node->rightChild() == 0)
+                {
+                    parent->left = node->left;
+                }
+                //Right child
+                else if(node->leftChild() == 0 && node->rightChild() == 1)
+                {
+                    parent->left = node->right;
+                }
+                //Has two children
+                else if(node->leftChild() == 1 && node->rightChild() == 1)
+                {
+                    Node* temp = new Node(0); // 0 as temporary key
+                    Node* successor = new Node(0);
+                    successor = minNode(node->right);
+
+                    deleteNode(node->right, successor->key, node);
+                    node->key = successor->key;
+                    return ;
+                }
+            }
+            
+            //Pretty much identical to fLeft
+            else if(fRight)
+            {
+                //No children
+                if(node->leftChild() == 0 && node->rightChild() == 0)
+                {
+                    parent->right = nullptr;
+                }
+                //Left Child
+                else if(node->leftChild() == 1 && node->rightChild() == 0)
+                {
+                    parent->right = node->left;
+                }
+                //Right child
+                else if(node->leftChild() == 0 && node->rightChild() == 1)
+                {
+                    parent->left = node->right;
+                }
+                //Has two children
+                else if(node->leftChild() == 1 && node->rightChild() == 1)
+                {
+                    Node* successor = new Node(0);
+                    successor = minNode(node->right);
+
+                    deleteNode(node->right, successor->key, node);
+                    node->key = successor->key;
+                    return ;
+                }
+            }
+
+        }
+        here:
+        if(key != node->key || enc < encounter)
+        {
+            parent = node;
+            if(key <= node->key)
+            {
+                deleteNode(node->left, key, parent);
+            }
+            else if(key > node->key)
+            {
+                deleteNode(node->right, key, parent);
+            }
+        }
+
     }
 
     void inorder(Node* root)
@@ -118,23 +217,31 @@ int main()
     BinaryTree bt;
     vector<Node*> nodes;
 
-    vector<int> keys = {5, 3, 8, 2, 4};
+    vector<int> keys = {8, 4, 2, 6, 5, 7, 10, 9, 15, 12, 20};
     for(int i = 0; i < keys.size(); ++i)
     {
         Node* node = new Node(keys[i]);
         nodes.push_back(node);
         bt.insert(nodes[i], bt.root);
     }
+
+    bt.printTree2D(bt.root, 2);
+    cout << "Inorder Traversal:";
+    bt.inorder(bt.root); cout << endl;
+
     
-    cout << "Node list: ";
-    for(int i = 0; i < nodes.size(); ++i)
-     cout << nodes[i]->key << " ";
-    cout << endl;
+    //If you want to delete a number whose key
+    //appears multiple times in the tree,
+    //Add parameter: bt.root, occurence_count
+    //Eg: Delete node with key = 5 that appears the second time: 
+    //  bt.deleteNode (bt.root, 5, bt.root, 2);
+    int delete_key = 4;
+    cout << "Deleting number " << delete_key << endl;
+    bt.deleteNode(bt.root, delete_key);
 
     bt.printTree2D(bt.root, 2);
     cout << "Inorder Traversal:";
     bt.inorder(bt.root);
 
-    
     return 0;
 }
