daafa5b7e956318aab59822ecf18ebbaf80a97c2
Minh Hoang <hoangdsieud@gmail.com>
2021-07-02 20:20:53 +0700

    Finished Red Black Tree
    Tested with many cases. Re-read for better readability.

--- Hoang/red_black_tree.cpp
+++ Hoang/red_black_tree.cpp
@@ -1,7 +1,7 @@
 /*
  * Red Black Tree
  * 
- * 1st July
+ * 2nd July
  * Teresuki
 */
 
@@ -28,29 +28,22 @@ struct Node
         parent = nullptr;
         color = red;
     }
-
-    bool leftChild()
-    {
-        if(left != nullptr){
+    bool leftChild(){
+        if(left != nullptr)
             return true;
-        }
         else return false;
     }
-
-    bool rightChild()
-    {
-        if(right != nullptr){
+    bool rightChild(){
+        if(right != nullptr)
             return true;
-        }
         else return false;
     }
 };
 
-
 struct RedBlackTree
 {
     Node* root;
-    Node* NIL = new Node(-1);
+    Node* NIL = new Node(-1); // NIL nodes' value are given default as -1
     int blackHeigth;
 
     RedBlackTree()
@@ -60,202 +53,142 @@ struct RedBlackTree
         NIL->left = nullptr;
         NIL->right = nullptr;
     }
-
-    Node* minNode(Node* node)
-    {
+    
+    void printBlackHeigth(){
+        Node* temp = new Node(0);
+        temp = root;
+        int bHeight = 0;
+        while(temp->left != nullptr){
+            temp = temp->left;
+            if(temp->color == black) ++bHeight;
+        }
+        blackHeigth = bHeight;
+        cout << "Black Height of this tree is: " << bHeight << endl;
+    }
+    Node* minNode(Node* node){
         Node* temp = node;
         while(temp->leftChild())
          temp = temp->left;
         return temp;
     }
-    Node* maxNode(Node* node)
-    {
+    Node* maxNode(Node* node){
         Node* temp = node;
         while(temp->rightChild())
          temp = temp->right;
         return temp;
     }
 
-    Node* getNode(int key, Node* rootR)
-    {
-        if(key < rootR->key)
-         getNode(key, rootR->left);
-        else if(key > rootR->key)
-         getNode(key, rootR->right);
-        else if(key == rootR->key)
-         return rootR;
-    }
-
-    void leftRotate(Node* gp, Node* parent, Node* node, bool NodeFLeft)
-    {
+    void leftRotate(Node* gp, Node* parent, Node* node, bool NodeFLeft){
       Node* gpParent = new Node(0);
       gpParent = gp->parent;
-    
+
+      if(gp == root) root = parent; // Update the root in the binary tree
+      if(gpParent != nullptr){
+        if(gp == gpParent->left)
+         gpParent->left = parent;
+        else
+         gpParent->right = parent;
+      }
+
       //Line
       if(!NodeFLeft)
       {
-          if(gpParent != nullptr)
-          {
-              if(gp == gpParent->left)
-               gpParent->left = parent;
-              else
-               gpParent->right = parent;
-          }
           gp->parent = parent;
-          gp->right = NIL;
+          gp->right = parent->left;
           parent->left = gp;
           parent->parent = gpParent;
-        //Recoloring
-         gp->color = red;
-         parent->color = black;
-         if(gp == this->root)
-         this->root = parent;
       }
       //Triangle
       else
       {
-          if(gpParent != nullptr)
-          {
-              if(gp == gpParent->left)
-               gpParent->left = node;
-              else
-               gpParent->right = node;
-          }
-          node->right = parent;
-          node->left = gp;
-          parent->parent = node;
-          parent->left = NIL;
-          gp->parent = node;
-          gp->right = NIL;
-          //Recoloring
-          gp->color = red;
-          node->color = black;
-          if(gp == this->root)
-           this->root = node;
+          gp->parent = parent;
+          gp->right = node;
+          parent->left = gp;
+          parent->parent = gpParent;
+          node->parent = gp;
       }
-
     }
 
-    void rightRotate(Node* gp, Node* parent, Node* node, bool NodeFLeft)
-    {
+    void rightRotate(Node* gp, Node* parent, Node* node, bool NodeFLeft){
       Node* gpParent = new Node(0);
       gpParent = gp->parent;
-    
+      
+      if(gp == root) root = parent; // Update the root in the binary tree
+      if(gpParent != nullptr){
+        if(gp == gpParent->left)
+         gpParent->left = parent;
+        else
+         gpParent->right = parent;
+      }
+
       //Line
-      if(NodeFLeft)
-      {
-          if(gpParent != nullptr)
-          {
-              if(gp == gpParent->left)
-               gpParent->left = parent;
-              else
-               gpParent->right = parent;
-          }
+      if(NodeFLeft){
           gp->parent = parent;
           gp->left = NIL;
           parent->right = gp;
           parent->parent = gpParent;
-        //Recoloring
-         gp->color = red;
-         parent->color = black;
-         if(gp == this->root)
-         this->root = parent;
       }
       //Triangle
-      else
-      {
-          if(gpParent != nullptr)
-          {
-              if(gp == gpParent->left)
-               gpParent->left = node;
-              else
-               gpParent->right = node;
-          }
+      else{
           node->left = parent;
           node->right = gp;
           parent->parent = node;
           parent->right = NIL;
           gp->parent = node;
           gp->left = NIL;
-          //Recoloring
-          gp->color = red;
-          node->color = black;
-          if(gp == this->root)
-           this->root = node;
       }
-
     }
 
-    void insert_fix(Node* node, bool NodeFLeft)
-    {
-        
+    void insert_fix(Node* node, bool NodeFLeft){
         Node* parent = new Node(0);
+        Node* gp = new Node(0);
+        Node* uncle = new Node(0);
+
         parent = node->parent;
+        gp = parent->parent;
 
-        if(node->color == black) return ;
-
-        if(parent->color == red)
-        {
-            //gp == Grandparent
-            Node* gp = new Node(0);
-            gp = parent->parent;
-
-            Node* uncle = new Node(0);
-
-            //Parent From Left
-            bool ParentFLeft = (parent == gp->left);
-
-            //If parent is the left child of gp
-            if(ParentFLeft)
-             uncle = gp->right; //Uncle is the right child of gp
-
-            //If parent is the right child of gp
-            else
-             uncle = gp->left;//Uncle is the left child of gp
+        //Parent is the left child of Grandparent
+        bool ParentFLeft = (parent == gp->left);
+        if(ParentFLeft)
+         uncle = gp->right; 
+        else
+         uncle = gp->left;
             
-            //If the uncle is black
-            if(uncle->color == black)
-            {
-                
-                if(ParentFLeft)
-                 rightRotate(gp, parent, node, NodeFLeft);
-                else
-                 leftRotate(gp, parent, node, NodeFLeft);
-
-            }
-            else if(uncle->color == red)
-            {
-                parent->color = black;
-                uncle->color = black;
-                gp->color = red;
-            }
+        if(uncle->color == black){
+            if(ParentFLeft)
+             rightRotate(gp, parent, node, NodeFLeft);        
+            else
+             leftRotate(gp, parent, node, NodeFLeft);
+            //Recoloring
+            gp->color = red;
+            parent->color = black;  
+        }
+        else{
+            //Recoloring
+            parent->color = black;
+            uncle->color = black;
+            gp->color = red;
 
             if(root->color == red)
              root->color = black;
-            
-            if(parent->parent != nullptr)
-             {
-                 NodeFLeft = (parent == (parent->parent->left) );
-                 insert_fix(parent, NodeFLeft);
+
+            if(gp->parent != nullptr){
+              if(gp->parent->color == red)
+              insert_fix(gp, (gp == gp->parent->left) );
              }
-            
         }
-        
     }
 
-    void insert(Node* node, Node* rootR)
-    {
+    void insert(Node* node, Node* rootR){
         //There is no root node yet
-        if(root == nullptr)
-        {
+        if(root == nullptr){
             root = node;
             root->color = black;
             root->left = NIL;
             root->right = NIL;
         }
         //There is already a root node
-        else
-        {
+        else{
             //If the key is smaller than the node's key
             if(node->key <= rootR->key)
             {
@@ -263,36 +196,35 @@ struct RedBlackTree
                 if(rootR->leftChild() && rootR->left != NIL)
                  insert(node, rootR->left);
                 
-                else
-                 {
+                else{
                     rootR->left = node;
                     node->parent = rootR;
                     node->left = NIL;
                     node->right = NIL;
-                    insert_fix(node, true);
+                    if(rootR->color == red)
+                     insert_fix(node, true);
                  }
             }
             //If the key is larger than the node's key
-            else if(node->key > rootR->key)
+            else
             {
                 //If there is a child on the right and it's not NIL
                 if(rootR->rightChild() && rootR->right != NIL)
                  insert(node, rootR->right);
                 
-                else
-                 {
+                else{
                     rootR->right = node;
                     node->parent = rootR;
                     node->left = NIL;
                     node->right = NIL;
-                    insert_fix(node, false);
+                    if(rootR->color == red)
+                     insert_fix(node, false);
                  }
             }
         }
     }
 
-    void inorder(Node* root)
-    {
+    void inorder(Node* root){
         if(root->leftChild()){
             inorder(root->left);
         }
@@ -305,8 +237,7 @@ struct RedBlackTree
         }
     }
 
-   void printTree2D(Node* node, int space)
-   {
+   void printTree2D(Node* node, int space){
        if (node == NULL)
         return ;
        space += 2;
@@ -315,22 +246,24 @@ struct RedBlackTree
        cout << endl;
        for(int i = 1; i < space; ++i)
         cout << "   ";
+
         if(node->color == red) cout << "(R)";
         else                   cout << "(B)";
-       cout << node->key << endl;
+
+        if(node->key == -1) cout << "NIL" << endl;
+        else cout << node->key << endl;
 
        printTree2D(node->left, space);
    }
-
 };
-
 
 int main()
 {
     RedBlackTree rbt;
     vector<Node*> nodes;
 
-    vector<int> keys = {8, 4, 2, 6, 5, 7};
+    //Change your input here:
+    vector<int> keys = {8, 4, 2, 6, 5, 7, 1};
     for(int i = 0; i < keys.size(); ++i)
     {
         Node* node = new Node(keys[i]);
@@ -341,7 +274,7 @@ int main()
     rbt.printTree2D(rbt.root, 2);
     cout << "Inorder Traversal:";
     rbt.inorder(rbt.root); cout << endl;
-
+    rbt.printBlackHeigth();
 
     return 0;
 }
